c60338ceb6495ea8d3c06598a71d9d416d39c4a7
Nefunguje_pero <90507546+Ricardoxdd@users.noreply.github.com>
2024-06-08 00:50:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// This script changes the content of the <h1> element with id 'greeting'
+document.addEventListener("DOMContentLoaded", function() {
+    const greetingElement = document.getElementById("greeting");
+    greetingElement.innerText = "Hello, World!";
+});
