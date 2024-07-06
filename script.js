af6e6ff71dd5e7fa93c633deec8f9cf52d336849
ondriric <ondriric@fit.cvut.cz>
2024-07-07 01:10:17 +0200

    Reloaded

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-// This script changes the content of the <h1> element with id 'greeting'
-document.addEventListener("DOMContentLoaded", function() {
-    const greetingElement = document.getElementById("greeting");
-    greetingElement.innerText = "Hello, World!";
-});
+class ButtonHandler {
+    constructor(buttonId, demoId) {
+        this.button = document.getElementById(buttonId);
+        this.demo = document.getElementById(demoId);
+        this.isClicked = false;
+        this.button.addEventListener('click', () => this.handleClick());
+    }
 
+    handleClick() {
+        if (!this.isClicked) {
+            this.demo.innerHTML = "Random number -> " + this.randomInt(1, 100);
+            document.body.style.backgroundColor = "#87EF62F9";
+            document.body.style.opacity = "0.7";
+            this.isClicked = true;
+        } else {
+            this.demo.innerHTML = "Test";
+            document.body.style.backgroundColor = "lightblue";
+            document.body.style.opacity = "1";
+            this.isClicked = false;
+        }
+    }
 
+    randomInt(number, number2) {
+        number = Math.ceil(number);
+        number2 = Math.floor(number2);
+        return Math.floor(Math.random() * (number2 - number + 1)) + number;
+    }
+}
+
+new ButtonHandler('myButton', 'demo');
